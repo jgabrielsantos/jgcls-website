55cfc4c291e0a8f0e73204fa51a2e0e00ea43088
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2024-01-23 15:49:14 -0300

    feat: projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,80 @@
+import { Card, Chip } from "@/components"
+import { Static } from "./static"
+import { imageList } from "@/utils/imageList"
+
+const Projects = () => {
+  return (
+    <main className="
+      w-full
+      p-5
+      flex
+      flex-col
+      gap-4
+      items-start
+
+      tablet:pt-24
+      tablet:px-10
+      tablet:grid
+      tablet:grid-cols-2
+      tablet:gap-x-14
+      tablet:gap-y-8
+    ">
+      <h1 className="
+        text-3xl
+        font-bold
+        text-white-100
+
+        tablet:text-5xl
+        tablet:col-span-2
+      ">
+        {Static.TITLE}
+      </h1>
+      {Static.PROJECTS.map((project, index) => (
+        <Card key={index}>
+          <div className="w-full flex justify-start items-center gap-4">
+            <h2 className="
+              text-2xl
+              font-semibold
+              text-white-100
+
+              tablet:text-3xl
+            ">
+              {project.NAME}
+            </h2>
+            <a
+              href={project.LINK}
+              target="_blank"
+            >
+              <img src={imageList.ExternalTabIcon.src} alt={imageList.ExternalTabIcon.alt} />
+            </a>
+          </div>
+          <div className='flex flex-wrap gap-x-1.5 gap-y-2 my-3 tablet:gap-3'>
+            {project.TECH.map((item, index) => (
+              <Chip key={index} name={item} />
+            ))}
+          </div>
+          <p className="
+            text-base
+            text-normal
+            text-white-100
+
+            tablet:text-xl
+          ">
+            {project.INTRO}
+          </p>
+          <p className="
+            text-base
+            text-normal
+            text-white-100
+
+            tablet:text-xl
+          ">
+            {project.DESCRIPTION}
+          </p>
+        </Card>
+      ))}
+    </main>
+  )
+}
+
+export default Projects
---
+++ src/app/projects/static.ts
@@ -0,0 +1,60 @@
+import { imageList } from "@/utils/imageList";
+
+export const Static = {
+  TITLE: 'Projects',
+  PROJECTS: [
+    {
+      NAME: 'Awesome GCL',
+      LINK: 'https://www.npmjs.com/package/awesome-gcl',
+      TECH: [
+        'TypeScript',
+        'Jest',
+        'React',
+        'Rollup',
+        'Tailwind CSS'
+      ],
+      INTRO: 'React components library',
+      DESCRIPTION: 'Planned, built, and maintain the foundational components library. This library provides thoroughly tested, ready-to-use components, streamlining both planning and development processes, and ensuring ongoing efficiency and quality.'
+    },
+    {
+      NAME: 'Fintel Connect',
+      LINK: 'https://app.fintelconnect.com/login',
+      TECH: [
+        'TypeScript',
+        'JavaScript',
+        'React',
+        'AM Charts',
+        'Styled Components',
+        'GraphQL',
+        'Express',
+        'MongoDB'
+      ],
+      INTRO: 'Affiliate Platform',
+      DESCRIPTION: 'Led the complete refactoring of the frontend, ensuring optimal performance and backend integration, while updating APIs for improved response times.'
+    },
+    {
+      NAME: 'Convergence Concepts',
+      LINK: 'https://cctech.io/',
+      TECH: [
+        'TypeScript',
+        'Next.js',
+        'Contentful',
+        'Styled Components',
+        'Yup'
+      ],
+      INTRO: 'Marketing website',
+      DESCRIPTION: 'Managed the technical and code aspects of the company\'s website redesign, focusing on modernization and efficiency.'
+    },
+    {
+      NAME: 'Giant Leap',
+      LINK: 'https://www.giantleap.ai/',
+      TECH: [
+        'TypeScript',
+        'Next.js',
+        'Styled Components',
+      ],
+      INTRO: 'Marketing website',
+      DESCRIPTION: 'Executed the technical planning and development of the platform, including integration with a Text Generation API for enhanced functionality.'
+    }
+  ]
+}
